d676de98fda4daeb3e3557e87ddbccc4a3236bb9
qkmBit <83538106+qkmBit@users.noreply.github.com>
2021-05-02 15:52:22 +0300

    Add files via upload

---
+++ Example1/Example1NoRefactoring.cs
@@ -0,0 +1,277 @@
+﻿using System;
+using System.Net;
+using System.IO;
+using System.Collections.Generic;
+
+namespace LR8._2
+{
+    class LR
+    {
+        int konec;
+        string vixod;
+
+        public int Konec { get => konec; set => konec = value; }
+        public string Vixod { get => vixod; set => vixod = value; }
+
+        public string getSubString(LR into, string left, string right, int start)
+        {
+            string back = null;
+            char[] Vxod = into.vixod.ToCharArray();
+            char[] Left = left.ToCharArray();
+            char[] Right = right.ToCharArray();
+            bool flag = true;
+            int count = 0;
+            for (int i = start; i < Vxod.Length || flag; i++) //В первом цикле перебираем значения массива символов 
+            {
+                count = 0;
+                for (int j = 0; j < Left.Length; j++) //Во втором цикле поэлементно сравниваем с левой границей 
+                {
+                    if (Vxod[i] == Left[j])
+                    {
+                        i++;
+                        count++;
+                    }
+                    else break;
+                    if (count == Left.Length) //Если нашли левую границу, то начинаем записывать символу в новую строку, пока не найдем правую границу
+                    {
+                        for (int k = i; flag; k++)
+                        {
+                            count = 0;
+                            for (int f = 0; f < Right.Length; f++) //Поэлементно сравниваем символы исходной строки с символами правой границы, пока её не найдем
+                            {
+                                if (Vxod[k] == Right[f])
+                                {
+                                    k++;
+                                    count++;
+                                }
+                                else break;
+                                if (count == Right.Length)
+                                {
+                                    flag = false;
+                                    into.konec = k;
+                                }
+                            }
+                            if (flag == true)
+                            {
+                                back += Vxod[k];
+                            }
+                        }
+                    }
+                }
+            }
+            return back;
+        }
+        public int GetSum(LR into, string left, string right, int start) // Находим сумму баллов
+        {
+            int sum = 0;
+            int next = start;
+            for (int i = 0; i < 8; i++) //Суммируем 8 строк, найденных методом getSubString
+            {
+                sum += int.Parse(into.getSubString(into, left, right, next));
+                next = into.Konec;
+            }
+            return sum;
+        }
+    }
+    class Program
+    {
+        static string GET(string type, int page)
+        {
+            WebRequest request = WebRequest.Create("http://playpit.ru/220/tableResultGET.php?type=" + type+"&page="+page) ;
+            WebResponse response = request.GetResponse();
+            Stream stream = response.GetResponseStream();
+            string info;
+            using (StreamReader reader = new StreamReader(stream))
+            {
+                info=reader.ReadToEnd();
+            }
+            stream.Close();
+            response.Close();
+            return info;
+        }
+        static int EndOfInputString(string vxod, string left, string right, int start)
+        {
+            char[] Vxod = vxod.ToCharArray();
+            char[] Left = left.ToCharArray();
+            char[] Right = right.ToCharArray();
+            bool flag = true;
+            int count = 0;
+            int xxx=0;
+            for (int i = start; i<Vxod.Length|| flag; i++)
+            {
+                count = 0;
+                for (int j = 0; j < Left.Length; j++)
+                {
+                    if (Vxod[i] == Left[j])
+                    {
+                        i++;
+                        count++;
+                    }
+                    else break;
+                    if (count == Left.Length)
+                    {
+                        for (int k = i; flag; k++)
+                        {
+                            count = 0;
+                            for (int f = 0; f < Right.Length; f++)
+                            {
+                                if (Vxod[k] == Right[f])
+                                {
+                                    k++;
+                                    count++;
+                                }
+                                else break;
+                                if (count == Right.Length)
+                                    flag = false;
+                            }
+                                xxx = k;
+                        }
+                    }
+                }
+            }
+            return xxx;
+        }
+        static string getSubString (string vxod, string left, string right, int start)
+        {
+            string back=null;
+            char[] Vxod = vxod.ToCharArray();
+            char[] Left = left.ToCharArray();
+            char[] Right = right.ToCharArray();
+            bool flag = true;
+            int count=0;
+            for (int i = start; i<Vxod.Length||flag; i++) //В первом цикле перебираем значения массива символов 
+            {
+                count = 0;
+                for(int j = 0; j < Left.Length; j++) //Во втором цикле поэлементно сравниваем с левой границей 
+                {
+                    if (Vxod[i] == Left[j])
+                    {
+                        i++;
+                        count++;
+                    }
+                    else break;
+                    if (count == Left.Length) //Если нашли левую границу, то начинаем записывать символу в новую строку, пока не найдем правую границу
+                    {
+                        for (int k = i; flag ; k++)
+                        {
+                            count = 0;
+                            for (int f = 0; f < Right.Length; f++) //Поэлементно сравниваем символы исходной строки с символами правой границы, пока её не найдем
+                            {
+                                if (Vxod[k] == Right[f])
+                                {
+                                    k++;
+                                    count++;
+                                }
+                                else break;
+                                if (count == Right.Length)
+                                {
+                                    flag = false;
+                                    
+                                }
+                            }
+                            if (flag == true)
+                            {
+                                back += Vxod[k];
+                            }
+                        }
+                    }
+                }
+            }
+            return back;
+        }
+        static int GetEnd(string vxod, string left, string right, int start)
+        {
+            int next = start;
+            for (int i = 0; i < 8; i++)
+            {
+                next = EndOfInputString(vxod, left, right, next);
+            }
+            return next;
+        }
+        static int GetSum(string vxod, string left, string right, int start) // Находим сумму баллов
+        {
+            int sum=0;
+            int next=start;
+            for (int i = 0; i < 8; i++) //Суммируем 8 строк, найденных методом getSubString
+            {
+                sum += int.Parse(getSubString(vxod, left, right, next)); 
+                next = EndOfInputString(vxod, left, right, next);
+            }
+            return sum;
+        }
+        static void Main(string[] args)
+        {
+            List<string[]> list = new List<string[]>();
+            string labi= "";
+            string practiki = "";
+            for(int i = 0; i<4; i++)
+            {
+                var lab = GET("lb", i);
+                var pract = GET("pr", i);
+                labi += lab;
+                labi += "\n";
+                practiki += pract;
+                practiki += "\n";
+            }
+            string[,] lablist = new string[20, 4]; //Создаем двумерный массив [20,4], где 20 - кол-во студентов в группе, 4 - колв-во полей
+            int nextl = 0;
+            int nextp = 0;
+            LR test = new LR();
+            test.Vixod = labi;
+            LR practs = new LR();
+            practs.Vixod = practiki;
+            for (int i = 0; i < 19; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    if (j == 0) //1-e поле - ФИО
+                    {
+                        lablist[i, j] = test.getSubString(test, "<td>", "</td>", nextl);
+                        nextl = test.Konec;
+                        practs.getSubString(practs, "<td>", "</td>", nextp);
+                        nextp = practs.Konec;
+                    }
+                    else if (j == 1) //2-е поле - балл по лабам
+                    {
+                        lablist[i, j] = test.GetSum(test, "<td>", "</td>", nextl).ToString();
+                        nextl = test.Konec;
+                    }
+                    else if (j == 2) //3-е поле - балл по практикам
+                    {
+                        lablist[i, j] = practs.GetSum(practs, "<td>", "</td>", nextp).ToString();
+                        nextp = practs.Konec;
+                    }
+                    else if (j == 3) //4-е поле - сумма баллов
+                    {
+                        int k = int.Parse(lablist[i, 1]) + int.Parse(lablist[i, 2]);
+                        lablist[i, j] = k.ToString();
+                    }
+                }
+            }
+            for (int j = 0; j < 19; j++) //Сортировка пузырьком по сумме баллов
+            {
+                for (int i =0 ; i < 18-j; i++)
+                {
+                    if (int.Parse(lablist[i, 3]) < int.Parse(lablist[i + 1, 3]))
+                    {
+                        for (int k = 0; k < 4; k++)
+                        {
+                            string temp = lablist[i, k];
+                            lablist[i, k] = lablist[i + 1, k];
+                            lablist[i + 1, k] = temp;
+                        }
+                    }
+                }
+            }
+            Console.WriteLine(".____________________________________________________________________.");
+            Console.WriteLine("|{0, -35}|{1, -10}|{2, -10}|{3, -10}|", "ФИО", "Лаб", "Практ", "Сумма");
+            Console.WriteLine("|___________________________________|__________|__________|__________|");
+            for (int i = 0; i < 19; i++)
+            {
+                Console.Write("|{0, -35}|{1, -10}|{2, -10}|{3, -10}|", lablist[i,0], lablist[i,1], lablist[i,2], lablist[i,3]);
+                Console.WriteLine();
+            }
+            Console.WriteLine(".____________________________________________________________________.");
+        }
+    }
+}
---
+++ Example2/Example2NoRefactoring.cs
@@ -0,0 +1,226 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+using System.IO.Compression;
+
+namespace whitezv
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path;
+            Console.Write("Введите путь:\t");
+            while (true)
+            {
+                path = Console.ReadLine();
+                if (Regex.IsMatch(path, @"\S:\\\w", RegexOptions.IgnoreCase))
+                {
+                    break;
+                }
+            }
+            DirectoryInfo directory = new DirectoryInfo(path);
+            Regex regex = new Regex(@" \w{8}-\w{4}-\w{4}-\w{4}-\w{12}");
+            if (directory.Exists)
+            {
+                DirectoryInfo[] dirs = directory.GetDirectories();
+                string[] dUUID = new string[dirs.Length];
+                string[] d = new string[dUUID.Length];
+                for (int i = 0; i < dirs.Length; i++)
+                {
+                    dUUID[i] = dirs[i].Name;
+                }
+                for (int i = 0; i < dirs.Length; i++)
+                {
+                    d[i] = dirs[i].Name;
+                }
+                Array.Sort(dUUID);
+                for (int i = 0; i < dirs.Length; i++)
+                {
+
+                    dUUID[i] = regex.Replace(dirs[i].Name, "");
+                }
+                for (int i = 1; i < dirs.Length; i++)
+                {
+                    if (dUUID[i] == dUUID[i - 1])
+                    {
+                        FileInfo[] filesInfo1 = dirs[i].GetFiles();
+                        FileInfo[] fileInfo2 = dirs[i - 1].GetFiles();
+                        if (fileInfo2.Length != filesInfo1.Length)
+                        {
+                            int p = 1;
+                            if (Directory.Exists(path + "\\" + dUUID[i]) == false)
+                                dirs[i - 1].MoveTo(path + "\\" + dUUID[i]);
+                            while (true)
+                            {
+                                dUUID[i] = dUUID[i] + "(" + p + ")";
+                                if (Directory.Exists(path + "\\" + dUUID[i]))
+                                    p++;
+                                else
+                                {
+                                    dirs[i].MoveTo(path + "\\" + dUUID[i]);
+                                    break;
+                                }
+                            }
+                        }
+                        else
+                        {
+                            foreach (FileInfo file in fileInfo2)
+                            {
+                                foreach (FileInfo fileInfo in filesInfo1)
+                                {
+                                    if (file.Name == fileInfo.Name)
+                                    {
+                                        string Txt, Txt1;
+                                        using (FileStream fstrem = File.OpenRead($"{path}\\{d[i - 1]}\\{file}"))
+                                        {
+                                            byte[] vs = new byte[fstrem.Length];
+                                            fstrem.Read(vs, 0, vs.Length);
+                                            Txt = System.Text.Encoding.Default.GetString(vs);
+                                        }
+                                        using (FileStream fstrem = File.OpenRead($"{path}\\{d[i]}\\{fileInfo}"))
+                                        {
+                                            byte[] vs = new byte[fstrem.Length];
+                                            fstrem.Read(vs, 0, vs.Length);
+                                            Txt1 = System.Text.Encoding.Default.GetString(vs);
+                                        }
+                                        if (Txt != Txt1)
+                                        {
+                                            int p = 1;
+                                            dirs[i - 1].MoveTo(path + "\\" + dUUID[i]);
+                                            while (true)
+                                            {
+                                                dUUID[i] = dUUID[i] + "(" + p + ")";
+                                                if (Directory.Exists(path + "\\" + dUUID[i]))
+                                                    p++;
+                                                else
+                                                {
+                                                    dirs[i].MoveTo(path + "\\" + dUUID[i]);
+                                                    break;
+                                                }
+                                            }
+                                            break;
+                                        }
+                                        else
+                                        {
+                                            dirs[i - 1].MoveTo(path + "\\" + dUUID[i]);
+                                            dirs[i].Delete(true);
+                                        }
+                                    }
+                                    else
+                                    {
+                                        int p = 1;
+                                        dirs[i - 1].MoveTo(path + "\\" + dUUID[i]);
+                                        while (true)
+                                        {
+                                            dUUID[i] = dUUID[i] + "(" + p + ")";
+                                            if (Directory.Exists(path + "\\" + dUUID[i]))
+                                                p++;
+                                            else
+                                            {
+                                                dirs[i].MoveTo(path + "\\" + dUUID[i]);
+                                                break;
+                                            }
+                                        }
+                                        break;
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+                DirectoryInfo[] directories = directory.GetDirectories();
+                string[] str = new string[directories.Length];
+                for (int i = 0; i < directories.Length; i++)
+                {
+                    str[i] = directories[i].Name;
+                    str[i] = regex.Replace(directories[i].Name, "");
+                }
+                for (int i = 0; i < str.Length; i++)
+                {
+                    if (Directory.Exists(path + "\\" + str[i]) == false)
+                    {
+                        directories[i].MoveTo(path + "\\" + str[i]);
+                    }
+                }
+                FileInfo[] files = directory.GetFiles();
+                string[] fUUID = new string[files.Length];
+                string[] f = new string[files.Length];
+                for (int i = 0; i < files.Length; i++)
+                {
+                    f[i] = files[i].Name;
+                    fUUID[i] = files[i].Name;
+                }
+                Array.Sort(fUUID);
+                for (int i = 0; i < files.Length; i++)
+                {
+                    fUUID[i] = regex.Replace(files[i].Name, "");
+                }
+                for (int i = 1; i < files.Length; i++)
+                {
+                    if (fUUID[i] == fUUID[i - 1])
+                    {
+                        string Txt, Txt1;
+                        using (FileStream fstrem = File.OpenRead($"{path}\\{f[i]}"))
+                        {
+                            byte[] vs = new byte[fstrem.Length];
+                            fstrem.Read(vs, 0, vs.Length);
+                            Txt1 = System.Text.Encoding.Default.GetString(vs);
+                        }
+                        using (FileStream fstrem = File.OpenRead($"{path}\\{f[i - 1]}"))
+                        {
+                            byte[] vs = new byte[fstrem.Length];
+                            fstrem.Read(vs, 0, vs.Length);
+                            Txt = System.Text.Encoding.Default.GetString(vs);
+                        }
+                        if (Txt != Txt1)
+                        {
+                            int p = 1;
+                            files[i - 1].MoveTo(path + "\\" + fUUID[i]);
+                            while (true)
+                            {
+                                string res;
+                                string[] rs = fUUID[i].Split('.');
+                                res = rs[0];
+                                for (int j = 1; j < rs.Length-2; j++)
+                                {
+                                    res = res + " " + rs[j];
+                                }
+                                res = res + " (" + p + ")." + rs[rs.Length-1];
+                                if (Directory.Exists(path + "\\" + fUUID[i]))
+                                    p++;
+                                else
+                                {
+                                    fUUID[i] = res;
+                                    files[i].MoveTo(path + "\\" + fUUID[i]);
+                                    break;
+                                }
+                            }
+                        }
+                    }
+                }
+                FileInfo[] filestest = directory.GetFiles();
+                string[] test = new string[filestest.Length];
+                for (int i = 0; i < files.Length; i++)
+                {
+                    test[i] = filestest[i].Name;
+                    test[i] = regex.Replace(filestest[i].Name, "");
+                }
+                for (int i = 0; i < test.Length; i++)
+                {
+                    if (File.Exists(path + "\\" + test[i]) == false)
+                    {
+                        filestest[i].MoveTo(path + "\\" + test[i]);
+                    }
+                }
+                string compressionPath = directory.Parent.FullName + @"\Utrom's secrets.zip";
+                ZipFile.CreateFromDirectory(path, compressionPath);
+                Console.ReadLine();
+            }
+        }
+    }
+}
